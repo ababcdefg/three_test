c2b72f694d6613204b08aa1c0808f3d6b21deb3c
lpchouse <2215437227@qq.com>
2024-04-12 17:22:26 +0800

    相机

--- src/index.js
+++ src/index.js
@@ -1,17 +1,82 @@
-import { Scene, PerspectiveCamera, BoxGeometry, MeshBasicMaterial, Mesh, WebGLRenderer } from 'three';
+import {
+  Scene, PerspectiveCamera, OrthographicCamera, BoxGeometry, MeshBasicMaterial, Mesh, WebGLRenderer, Vector3, AxesHelper, Group, Color, Clock,
+} from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import Gsap from 'gsap';
+
 
 const scene = new Scene()
-const viewSize = { width: 800, height: 600 }
-const camera = new PerspectiveCamera(45, viewSize.width / viewSize.height, 1, 1000)
-camera.position.z = 3
-scene.add(camera)
+scene.background = new Color(0.2, 0.2, 0.2)
 
-const geometry = new BoxGeometry(1, 1, 1)
-const material = new MeshBasicMaterial({ color: 0xffff00 })
-const cube = new Mesh(geometry, material)
-scene.add(cube)
+const viewSize = { width: 800, height: 600 } //视口比例
 
 const renderer = new WebGLRenderer()
 renderer.setSize(viewSize.width, viewSize.height)
 document.querySelector('#app')?.appendChild(renderer.domElement)
-renderer.render(scene, camera)
+
+//透视相机
+const camera = new PerspectiveCamera(75, viewSize.width / viewSize.height, 0.1, 1000)
+//正交相机
+// const aspectRatio = viewSize.width / viewSize.height
+// const camera = new OrthographicCamera(-3 * aspectRatio, 3 * aspectRatio, 3, -3, 0.1, 100)
+camera.position.set(0, 0, 5)
+scene.add(camera)
+// controls
+const controls = new OrbitControls(camera,renderer.domElement)
+controls.enableDamping = true   //启用相机控制器阻尼  (每一帧要调用控件的update更改相机状态)
+// controls.target.y = 1
+
+
+//axes helper
+const axesHelper = new AxesHelper(20)
+scene.add(axesHelper)
+
+const geometry = new BoxGeometry(1, 1, 1)
+const material = new MeshBasicMaterial({ color: 0xffff00 })
+const cube = new Mesh(geometry, material)
+// scene.add(cube)
+
+const cube2 = new Mesh(new BoxGeometry(1, 2, 1), material)
+cube2.position.set(-2, 0, -2)
+
+const group = new Group()
+scene.add(group)
+group.add(cube, cube2)
+
+//gasp动画库
+Gsap.to(cube2.position, { x: 2, duration: 1, delay: 1 })
+Gsap.to(cube2.position, { x: -2, duration: 1, delay: 2 })
+
+
+
+
+const clock = new Clock()   //时钟
+const tick = () => {
+  controls.update()
+
+  const elapsedTime = clock.getElapsedTime()   //时间戳(从0开始经历的秒数)
+  cube.position.y = Math.sin(elapsedTime)
+  cube.position.x = Math.cos(elapsedTime)
+  cube2.rotation.y = elapsedTime
+
+  renderer.render(scene, camera)
+  requestAnimationFrame(tick)
+}
+tick()
+
+// setTimeout(() => {
+//   cube.position.set(0.7, -0.6, -1)
+//   cube.scale.set(2, 0.5, 0.5)
+// }, 1000);
+
+// setTimeout(() => {
+//   // cube.position.length()      //向量长度
+//   // cube.position.normalize()  //向量归一化
+//   // cube.rotation.reorder('YXZ')  //更改旋转先后顺序
+//   cube.rotation.set(1, 1, 0)
+//   // camera.lookAt(cube.position) // 相机朝向
+// }, 2000);
+
+// setTimeout(() => {
+//   group.rotation.set(0, 0, 1)
+// }, 3000)
